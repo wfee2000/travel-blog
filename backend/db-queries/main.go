f53944152447bb2d6911cc2c8345392404e135ce
LukaCvijic <lukacvijic@outlook.com>
2024-04-10 14:28:09 +0200

    added mux dependencies and tested endpoints with http file

--- backend/db-queries/main.go
+++ backend/db-queries/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/gorilla/mux"
 	"io"
 	"net/http"
 	"os"
 )
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
+type Item struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+}
+
+func postHello(w http.ResponseWriter, r *http.Request) {
+	var item Item
+
+	err := json.NewDecoder(r.Body).Decode(&item)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(w).Encode(item)
 }
 func getHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got /hello request\n")
@@ -19,10 +35,10 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	mux := http.NewServeMux()
+	mux := mux.NewRouter()
 
-	mux.HandleFunc("/", getRoot)
-	mux.HandleFunc("/hello", getHello)
+	mux.HandleFunc("/hello", getHello).Methods("GET")
+	mux.HandleFunc("/post", postHello).Methods("POST")
 
 	err := http.ListenAndServe(":3333", mux)
 
