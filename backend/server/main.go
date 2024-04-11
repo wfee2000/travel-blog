f5f197278f1d966cb58be378338e16f77e3d8e7c
LukaCvijic <lukacvijic@outlook.com>
2024-04-11 12:11:14 +0200

    confirmed that post works

--- backend/server/main.go
+++ backend/server/main.go
@@ -28,17 +28,7 @@ func main() {
 }
 
 func postUser(context *gin.Context) {
-
-	context.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-	context.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-	context.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-	context.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-	fmt.Println("entered1")
-
 	controller.InitDbConnection()
-
-	fmt.Println("entered")
 
 	var user entities.BlogUser
 	err := context.BindJSON(&user)
