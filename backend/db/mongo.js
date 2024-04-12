52ed70e8c389c22ba9e30bed1636c2b59022e808
wfee2000 <ilmingwinnie19@gmail.com>
2024-04-12 21:49:50 +0200

    feat: add all update statements

--- backend/db/mongo.js
+++ backend/db/mongo.js
@@ -97,3 +97,52 @@ db.entries.aggregate([
         $project: { comments: { $slice: ["$comments.content", -2] } }
     }
 ])
+
+// Updates
+db.entries.updateOne(
+    {
+        _id: db.entries.aggregate({ $sort: { creationDate: -1 }}).toArray()[0]._id
+    },
+    { $set: { author_un: "charliebrown1" } }
+)
+db.entries.updateOne(
+    {
+        _id: db.entries.aggregate({ $sort: { creationDate: -1 }}).toArray()[0]._id
+    },
+    { $set: { hashtags: ["neu", "interessant"] } }
+)
+db.entries.updateMany(
+    { blogCategory: "Adventure" },
+    { $set: { blogCategory: "NotAdventure" } }
+)
+db.entries.updateOne(
+    {
+        title: "My title",
+    },
+    {
+        $set: {
+            title: "My title",
+            description: "My description",
+            author_un: "traveler123",
+            blogCategory: "Survival",
+            creationDate: new Date(),
+            editDates: [
+                new Date("2023-05-02"),
+                new Date("2024-01-30")
+            ],
+            impressionCount: 0,
+            contents: [
+                {
+                    type: "text",
+                    content: "Eastern Europe beckons with its allure, promising a journey through time and nature's wonders. Explore Krakow's medieval streets, Romania's rustic villages, and Croatia's enchanting lakes. From Gothic castles to thermal baths, adventure awaits in every corner. Ready to uncover Eastern Europe's secrets?"
+                }
+            ],
+            commentsAllowed: true,
+            comments: []
+        }
+    },
+    {
+        upsert: true
+    }
+)
+db.entries.deleteOne()
